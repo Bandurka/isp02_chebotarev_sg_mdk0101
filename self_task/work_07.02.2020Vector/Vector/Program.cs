5a5aac10df56e55bb4726d6a7b769ba426b365a2
Bandurka <chiebotariev2000@mail.ru>
2020-02-21 19:14:40 +0400

    work_07.02.2020Vector

---
+++ self_task/work_07.02.2020Vector/Vector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector = new Vector(4, 7, 8 );
+            Vector vector1 = new Vector(3, 5, 9);
+
+            Console.WriteLine(vector.ToString());
+            Console.WriteLine(vector1.ToString());
+
+            vector.Length();
+            Console.WriteLine($"Длина вектора: {vector.Length()}");
+
+            vector.ScalarProduct(vector1);
+            Console.WriteLine($"Скалярное произведение: {vector.ScalarProduct(vector1)}");
+
+            vector.VectorProduct(vector1);
+            Console.WriteLine($"Векторное произведение: {vector.VectorProduct(vector1)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ self_task/work_07.02.2020Vector/Vector/Vector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Vector
+    {
+        private double x { get; set; }
+        private double y { get; set; }
+        private double z { get; set; }
+
+        public Vector(double X, double Y, double Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+
+        public double Length()
+        {
+            double L = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+            return L;
+        }
+
+        public double ScalarProduct(Vector vector1)
+        {
+            double S = x * vector1.x + y * vector1.y + z * vector1.z;
+            return S;
+        }
+
+        public double VectorProduct(Vector vector1)
+        {
+            double X = (y * vector1.z - z * vector1.y);
+            double Y = (z * vector1.x - x * vector1.z);
+            double Z = (x * vector1.y - y * vector1.x);
+
+            
+            Vector vector2 = new Vector(X, Y, Z);
+            return X;
+            
+        }
+
+        public override string ToString()
+        {
+            return ($"vector: X: {x}, Y: {y}, Z: {z}");
+        }
+    }
+}
